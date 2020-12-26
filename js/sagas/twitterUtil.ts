4b836cfb899cb51c38cc1e02bba22f88e15d718b
pasta04 <aldente04@gmail.com>
2020-12-27 00:55:49 +0900

    [update] 改行のカウント方法をちゃんとした

--- js/sagas/twitterUtil.ts
+++ js/sagas/twitterUtil.ts
@@ -14,6 +14,7 @@ export const countStr = (baseStr: string): number => {
   // 半角：0.5文字。@hogehogeとかも同様のカウント。
   // 全角：1文字
   // URL：11.5文字
+  // 改行: 0.5文字
 
   // URLを含んでいる数
   const urls = baseStr.match(/https?:\/\/[^\s]+/g);
@@ -22,16 +23,19 @@ export const countStr = (baseStr: string): number => {
   // URLの部分を除外して文字数カウントする
   const pureStr = baseStr.replace(/https?:\/\/[^\s]+/g, '');
 
-  // TODO: 改行も0.5文字換算だけど以下の正規表現だとほんのちょっと多くカウントされちゃう
   // 半角文字
   const halfStrMatch = pureStr.match(/[ -~]+/g);
-  const halfStr = halfStrMatch ? halfStrMatch.join() : '';
+  const halfStr = halfStrMatch ? halfStrMatch.join('') : '';
 
   // 全角文字
-  const fullStr = pureStr.replace(/[ -~]+/g, ''); // 半角を全部除けば全角だけになるはず。たぶん
+  let fullStr = pureStr.replace(/[ -~]+/g, ''); // 半角を全部除くと全角と改行が残る
+  // 改行
+  const returMatch = fullStr.match(/\s/g);
+  const returnNum = returMatch? returMatch.length * 0.5 : 0;
+  fullStr = fullStr.replace(/\s/g, ''); // 全角だけにする
 
   // 合計文字数
-  const strLen = urlNum * 11.5 + halfStr.length * 0.5 + fullStr.length;
+  const strLen = urlNum * 11.5 + halfStr.length * 0.5 + fullStr.length + returnNum;
 
   return strLen;
 };
